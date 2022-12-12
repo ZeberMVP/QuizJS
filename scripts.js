68f025465ee6a5e7187c6eeefd57229e58c37f8a
ZeberMVP <rubeen722@gmail.com>
2022-12-12 20:24:46 +0100

    add/form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-document.querySelector('body').innerHTML = 'Holi!'
+const respuestas = {
+    año: "1997",
+    inventor: "Brendan Eich-field",
+    estándar: "ECMAScript 6",
+    organización: "Mozilla Foundation",
+    typeof: "Number",
+    framework: "Laravel",
+    empresa: "Oracle",
+    nombre: "Mocha",
+    tiempo: "10 días",
+    suma: "0.30000000000000004"
+}
